eeb2332bcae8fade4d7c08f74e1f5c820a2bed65
Viswanath Bodapati <81230933+Viswanath-621@users.noreply.github.com>
2023-12-21 23:37:34 +0530

    Add files via upload

---
+++ db/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    designation:{
+        type:String,
+        required:true,
+    },
+    district:{
+        type:String,
+        required:true,
+    }, 
+    transfer: {
+        type: [String], // Assuming transfer is an array of strings, adjust the type accordingly
+        default: [],
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      rcount:{
+        type:String,
+        required:true,
+      }  
+});
+
+const User = mongoose.model('User',UserSchema);
+
+module.exports = User;
